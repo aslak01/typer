b022fcc6d82eba69ec80877cb20ce827860d9ca1
as <aslak.bakkeland@medialaben.no>
2024-05-11 13:15:06 +0200

    refactor more

--- src/lib/server/book.ts
+++ src/lib/server/book.ts
@@ -1,7 +1,75 @@
 export const prerender = true;
 
-import chap1Raw from "$lib/data/books/gullivers/split/1-part1/part1.txt";
+export function getChapters(txt: string) {
+  console.log(typeof txt)
+  console.log(txt)
+  const chapters = txt.split(/(CHAPTER.+)\n/).filter(Boolean);
 
-console.log(chap1Raw);
+  type Chapter = {
+    title: string;
+    pages: string;
+  };
+  type Chapters = Chapter[];
 
-export { chap1Raw };
+  const pairs = chapters.reduce(
+    (result: Chapters, currentValue: string, index) => {
+      if (index % 2 === 0) {
+        const pair = {
+          "title": currentValue,
+          "pages": chapters[index + 1],
+        };
+
+        result.push(pair);
+      }
+      return result;
+    },
+    [],
+  );
+
+  function trimArrayWithRulesRecursive(
+    array: string[],
+    index = 0,
+    encounteredEmptyLine = true,
+  ): string[] {
+    if (index >= array.length) {
+      return [];
+    }
+
+    const str = array[index].trim();
+
+    if (str !== "") {
+      encounteredEmptyLine = false;
+      const rest = trimArrayWithRulesRecursive(array, index + 1, false);
+      return [str, ...rest];
+    } else {
+      if (!encounteredEmptyLine) {
+        encounteredEmptyLine = true;
+        const rest = trimArrayWithRulesRecursive(array, index + 1, true);
+        return [str, ...rest];
+      } else {
+        return trimArrayWithRulesRecursive(array, index + 1, true);
+      }
+    }
+  }
+
+  function trimFinalEmptyLines(arr: string[]): string[] {
+    const final = arr.at(-1);
+    if (final === "") arr.pop();
+    return arr;
+  }
+
+  function sepText(chapter: string) {
+    const trimmed = trimArrayWithRulesRecursive(chapter.split("\n"));
+    return trimFinalEmptyLines(trimmed);
+  }
+
+  const bookData = pairs.map((pair) => {
+    return {
+      "title": pair.title,
+      "pages": sepText(pair.pages),
+    };
+  });
+
+  return bookData;
+}
+
