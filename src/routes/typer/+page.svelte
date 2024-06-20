82c8548ba9abfce1f1f8e1d5a0e87de6fd9b171a
aslak <aslakrb@gmail.com>
2024-06-21 01:05:12 +0200

    layout shit

--- src/routes/typer/+page.svelte
+++ src/routes/typer/+page.svelte
@@ -5,11 +5,11 @@
 <ul>
   {#each bookIndex as book}
     <li class="book">
-      <img
-        src={`books/${book.path}/cover.jpg`}
-        alt="{book.title + ' cover'})"
-      />
       <a href={`/typer/${book.path}`}>
+        <img
+          src={`books/${book.path}/cover.jpg`}
+          alt="{book.title + ' cover'})"
+        />
         {book.author} - {book.title} ({book.year})
       </a>
     </li>
@@ -24,11 +24,15 @@
   }
   li {
     display: grid;
-    grid-template-columns: 80px 1fr;
-    align-items: center;
-    gap: 1em;
+    align-items: start;
+    padding-block-end: 5ch;
   }
   img {
     max-height: 150px;
+  }
+  a {
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    gap: 1em;
   }
 </style>
